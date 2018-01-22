1e83ffaa12630e2830823fe0a8fbb2288e17dcd7
VivianSnow <viviansnow@bupt.edu.cn>
2018-01-22 22:51:16 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), L"愤怒的小鸟");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Cyan);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::KeyPressed) {
+                if(event.key.code == sf::Keyboard::Escape)
+                    window.close();
+            }
+        }
+        shape.move(10, 0);
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
+}
